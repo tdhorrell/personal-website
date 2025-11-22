a1d50c63a302d977fb84735f47da30cc68b614ce
Tim Horrell <td.horrell11@gmail.com>
2025-11-22 11:11:28 -0500

    update file structure

---
+++ script.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+const navLinks = document.querySelectorAll('.nav-menu a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
+});
